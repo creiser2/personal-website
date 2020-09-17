ee3e25a8dd931bd735a37e6dff16d226662c85fd
creiser2 <carlreiser4@gmail.com>
2020-09-17 16:45:29 -0600

    starting about me page:

--- src/screens/AboutMePage.js
+++ src/screens/AboutMePage.js
@@ -4,17 +4,34 @@ import { Fragment, Component } from "react";
 import "../styles/AboutMePage.css";
 
 class AboutMePage extends Component {
-
-    downChevronClicked() {
-        // this.props.secondPageRef.scrollIntoView();
-        //this.refs.hello.scrollIntoView(); 
-    }
-
     render() {
         return (
             <Fragment>
-                <div className="about-me-container">
+                <div className="am-container">
+                    <div className="am-text-holder">
+                        <div className="am-spacer-1"></div>
+                        <div className="sf-semibold am-header">
+                            About Me
+                        </div>
+                        <div className="sf-thin-italic am-subtitle">
+                            I am a software engineer who has a passion for building the best
+                        </div>
+                        <div className="sf-thin-italic am-subtitle">
+                            possible product for the end user.
+                        </div>
+                    </div>
+                    <div className="am-button-container">
+                        <div className="am-button-filled sf-light">
+                            Development
+                        </div>
+                        <div className="am-spacer-2"></div>
+                        <div className="am-button-hollow sf-light">
+                            Deployment
+                        </div>
+                    </div>
+                    <div className="am-stack-container">
 
+                    </div>
                 </div>
             </Fragment>
         )
