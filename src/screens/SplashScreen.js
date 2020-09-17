f373431a38ff0055dcf62942a74d8edf102d873f
creiser2 <carlreiser4@gmail.com>
2020-09-17 10:41:45 -0600

    .. font fix 2

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/SplashScreen.css';
+import Constellations from "../shared/Constellations";
 import { Fragment } from 'react';
 function SplashScreen() {
     return (
@@ -8,6 +9,7 @@ function SplashScreen() {
                 <div className='bg2'>
                 </div>
             </div>
+            < Constellations/>
             <div className='splash-text sf-semibold center-xy'>
                 CR
             </div>
---
+++ src/shared/Constellations.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Constellations() {
+    return(
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Constellations;
