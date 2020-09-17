b4057a20346aad47f2c3478a66e62538e01bb2b7
creiser2 <carlreiser4@gmail.com>
2020-09-17 15:59:20 -0600

    scroll control

--- src/screens/Website.js
+++ src/screens/Website.js
@@ -8,10 +8,65 @@ class Website extends Component {
     constructor(props) {
         super(props);
         this.aboutMePageRef = React.createRef();
+        this.landingPageRef = React.createRef();
+        this.workTimelinePageRef = React.createRef();
+        this.contactMePageRef = React.createRef();
+
+        this.state = {
+            refs: {
+                landingPageRef: null,
+                aboutMePageRef: null,
+                workTimelinePageRef: null,
+                contactMePageRef: null,
+            }
+        }
     }
 
     componentDidMount() {
         document.body.style = ""
+        
+        var timer = null;
+        window.addEventListener('scroll', () => {
+            if (timer !== null) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(() => {
+                this.handleScroll()
+            }, 50);
+        }, false);
+
+        this.setState({
+            refs: {
+                landingPageRef: this.landingPageRef,
+                aboutMePageRef: this.aboutMePageRef,
+                workTimelinePageRef: this.workTimelinePageRef,
+                contactMePageRef: this.contactMePageRef,
+            }
+        })
+    }
+
+    // get the scroll height and the tops of all refs, and see if we are close to one to animate to
+    handleScroll = () => {
+        const lastScrollY = window.scrollY;
+        const threshold = 250;
+        
+        if (Math.abs(this.state.refs.landingPageRef.current._reactInternalFiber.child.stateNode.offsetTop - lastScrollY) <= threshold) {
+            this.scrollToLandingPage()
+        } else if (Math.abs(this.state.refs.aboutMePageRef.current._reactInternalFiber.child.stateNode.offsetTop - lastScrollY) <= threshold) {
+            this.scrollToAboutMe()
+        } else if (Math.abs(this.state.refs.workTimelinePageRef.current._reactInternalFiber.child.stateNode.offsetTop - lastScrollY) <= threshold) {
+            this.scrollToWorkTimelinePage()
+        } else if (Math.abs(this.state.refs.contactMePageRef.current._reactInternalFiber.child.stateNode.offsetTop - lastScrollY) <= threshold) {
+            this.scrollToContactMePage()
+        }
+    }
+
+    scrollToLandingPage = () => {
+        window.scrollTo({
+            top: this.landingPageRef.current._reactInternalFiber.child.stateNode.offsetTop,
+            left: 0,
+            behavior: 'smooth'
+        }); 
     }
 
     scrollToAboutMe() {
@@ -20,16 +75,32 @@ class Website extends Component {
             left: 0,
             behavior: 'smooth'
         });
-    } 
+    }
+    
+    scrollToWorkTimelinePage = () => {
+        window.scrollTo({
+            top: this.workTimelinePageRef.current._reactInternalFiber.child.stateNode.offsetTop,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    scrollToContactMePage = () => {
+        window.scrollTo({
+            top: this.contactMePageRef.current._reactInternalFiber.child.stateNode.offsetTop,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
 
     render() {
         return (
             <Fragment>
                 <Constellations />
-                <LandingPage aboutMeClicked={() => this.scrollToAboutMe()}/>
+                <LandingPage ref={this.landingPageRef} aboutMeClicked={() => this.scrollToAboutMe()}/>
                 <AboutMePage ref={this.aboutMePageRef} />
-                <WorkTimelinePage />
-                <ContactMePage />
+                <WorkTimelinePage ref={this.workTimelinePageRef}/>
+                <ContactMePage ref={this.contactMePageRef}/>
             </Fragment>
         )
     }
